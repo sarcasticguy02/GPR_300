9033b89a38343e91c9d2725f8b248d0ad9d73e5c
Andrew Feddersen <andrew.feddersen@mymail.champlain.edu>
2023-02-06 02:16:24 -0500

    model, view and projection matrix creating but nothing is changing on the when running

--- GPR300_Transformations_Template/GPR300_Transformations/main.cpp
+++ GPR300_Transformations_Template/GPR300_Transformations/main.cpp
@@ -18,7 +18,6 @@
 #include "EW/Shader.h"
 #include "EW/ShapeGen.h"
 
-#include <math.h>
 
 void resizeFrameBufferCallback(GLFWwindow* window, int width, int height);
 void keyboardCallback(GLFWwindow* window, int keycode, int scancode, int action, int mods);
@@ -43,17 +42,21 @@ const float MOUSE_SENSITIVITY = 0.1f;
 glm::vec3 bgColor = glm::vec3(0);
 float exampleSliderFloat = 0.0f;
 
-namespace matrises
-{
-	glm::mat4 vec3 transform, rotation, size;
+//UI Variables
+float radius = 10.0f, speed = 10.0f, fov = 10.0f, height = 10.0f;
+bool orthographic = false;
 
-	glm::mat4 scale(glm::vec3 s)
+struct Transform
+{
+	glm::vec3 pos = glm::vec3(0, 0, 0), rot = glm::vec3(0, 0, 0), scale = glm::vec3(1, 1, 1);
+
+	glm::mat4 size(glm::vec3 s)
 	{
 		return glm::mat4(
-			s.x, 0,  0,   0,
-			0,  s.y, 0,   0,
-			0,  0,  s.z,  0,
-			0,  0,   0,   1
+			s.x, 0, 0, 0,
+			0, s.y, 0, 0,
+			0, 0, s.z, 0,
+			0, 0, 0, 1
 		);
 	}
 
@@ -80,29 +83,90 @@ namespace matrises
 
 		return rotateX * rotateY * rotateZ;
 	}
-	
 
-	glm::mat4 translate(glm::vec3 t)
+
+	glm::mat4 position(glm::vec3 t)
 	{
 		return glm::mat4(
-			1,    0,   0,  0,   //column 0
-			0,    1,   0,  0,   //column 1
-			0,    0,   1,  0,   //column 2
+			1, 0, 0, 0,   //column 0
+			0, 1, 0, 0,   //column 1
+			0, 0, 1, 0,   //column 2
 			t.x, t.y, t.z, 1    //column 3
 		);
 	}
 
 	glm::mat4 getModelMatrix()
 	{
-		glm::mat4 rot = rotate(rotation);
-		glm::mat4 tran = translate(translation);
-		glm::mat4 scal = scale(size);
-
-		return scal * rot * tran;
+		return size(scale) * rotate(rot) * position(pos);
 	}
-}
+};
+
+struct Camera
+{
+	glm::vec3 camPos = glm::vec3(0, 0, 3), target = glm::vec3(0, 0, 0);
+	glm::mat4 getViewMatrix()
+	{
+		glm::vec3 forward = target - camPos;
+		glm::vec3 u = glm::vec3(0, 1, 0);
+		glm::vec3 right = cross(forward, u);
+		glm::vec3 up = cross(right, forward);
+
+		forward = -forward;
+
+		glm::mat4 rCam = glm::mat4(
+			right.x, up.x, forward.x, 0,
+			right.y, up.y, forward.y, 0,
+			right.z, up.z, forward.z, 0,
+			0, 0, 0, 1
+		);
+		glm::mat4 tCam = glm::mat4(
+			1, 0, 0, 0,
+			0, 1, 0, 0, 
+			0, 0, 1, 0,
+			-camPos.x, -camPos.y, -camPos.z, 1
+		);
+
+		return rCam * tCam;
+	};
+
+	glm::mat4 getProjectionMatrix()
+	{
+		if (orthographic)
+			return ortho(height, (float)(SCREEN_HEIGHT/SCREEN_WIDTH), .1f, 80.0f);
+		else
+			return perpective(height, (float)(SCREEN_HEIGHT / SCREEN_WIDTH), .1f, 80.0f);
+	};
+
+	glm::mat4 ortho(float h, float aspectRatio, float near, float far)
+	{
+		float w = aspectRatio * h;
+		float r = w / 2, t = h / 2;
+		float l = -r, b = -t;
+
+		return glm::mat4(
+			2 / (r - l), 0, 0, 0,
+			0, 2 / (t - b), 0, 0,
+			0, 0, -2 / (far - near), 0,
+			-(r + l) / (r - l), -(t + b) / (t - b), -(far + near) / (far - near), 1
+		);
+
+	};
+	glm::mat4 perpective(float fov, float aspectRatio, float near, float far)
+	{
+		float c = tan(fov / 2);
+			return glm::mat4(
+				1/aspectRatio*c, 0, 0, 0,   //column 0
+				0, 1/c, 0, 0,   //column 1
+				0, 0, -((far+near)/(far-near)), -1,   //column 2
+				0, 0, -((2*far*near)/(far-near)), 1    //column 3
+			);
+	};
+};
 
 int main() {
+	Transform trans;
+	Camera cam;
+
 	if (!glfwInit()) {
 		printf("glfw failed to init");
 		return 1;
@@ -161,15 +225,23 @@ int main() {
 
 		//Draw
 		shader.use();
-		for (size_t i = 0; i < 3; i++)
+		/*for (size_t i = 0; i < 3; i++)
 		{
-			shader.setMat4("_Model", transforms[i].getModelMatrix());
+			shader.setMat4("_Model", Transform.getModelMatrix());
 			cubeMesh.draw();
-		}
+		}*/
+		shader.setMat4("_Model", trans.getModelMatrix());
+		shader.setMat4("_View", cam.getViewMatrix());
+		shader.setMat4("_Projection", cam.getProjectionMatrix());
+		cubeMesh.draw();
 
 		//Draw UI
 		ImGui::Begin("Settings");
-		ImGui::SliderFloat("Example slider", &exampleSliderFloat, 0.0f, 10.0f);
+		ImGui::SliderFloat("Radius", &radius, 0.0f, 10.0f);
+		ImGui::SliderFloat("Speed", &speed, 0.0f, 10.0f);
+		ImGui::SliderFloat("FOV", &fov, 0.0f, 10.0f);
+		ImGui::SliderFloat("Orthographic Height", &height, 0.0f, 10.0f);
+		ImGui::Checkbox("Orthographic", &orthographic);
 		ImGui::End();
 
 		ImGui::Render();
